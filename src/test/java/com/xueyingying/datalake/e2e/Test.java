12eb74610994da7675c66d6a2cabb92028cf239c
xiaowu <1766128467@qq.com>
2025-06-12 08:54:16 +0800

    init

---
+++ src/test/java/com/xueyingying/datalake/e2e/Test.java
@@ -0,0 +1,12 @@
+package com.xueyingying.datalake.e2e;
+
+/**
+ * @author wuzh8@asiainfo.com
+ * @version 1.0.0
+ * @since 2025-06-11
+ */
+public class Test {
+  public static void main(String[] args) {
+    System.out.println(1111);
+  }
+}
